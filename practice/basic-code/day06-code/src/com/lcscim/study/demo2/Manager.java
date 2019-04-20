d973c74dd4a420e642408113ff26761ccf921961
lcscim <lvqihui@live.com>
2019-04-20 17:13:31 +0800

    0413 commit.

---
+++ practice/basic-code/day06-code/src/com/lcscim/study/demo2/Manager.java
@@ -0,0 +1,15 @@
+package com.lcscim.study.demo2;
+
+import java.util.ArrayList;
+
+public class Manager extends User {
+    public Manager() {
+    }
+
+    public Manager(String name, int money) {
+        super(name, money);
+    }
+    public ArrayList<Integer> send(int money,int count){
+
+    }
+}
